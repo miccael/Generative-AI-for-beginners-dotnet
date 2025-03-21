1ade5c5765196da2f3eed9821b84d79aef963894
Mikael Glans <mikaelglanz@hotmail.com>
2025-03-21 12:42:46 +0000

    02-SetupDevEnvironment_azureopenai

--- 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
+++ 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
@@ -1,11 +1,21 @@
 ﻿using Azure;
 using Azure.AI.Inference;
+
+
+using System.ClientModel;
+using Azure.AI.OpenAI;
 using Microsoft.Extensions.AI;
 
-IChatClient client = new ChatCompletionsClient(
-        endpoint: new Uri("https://models.inference.ai.azure.com"),
-        new AzureKeyCredential(Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? throw new InvalidOperationException("Missing GITHUB_TOKEN environment variable. Ensure you followed the instructions to setup a GitHub Token to use GitHub Models.")))
-        .AsChatClient("Phi-3.5-MoE-instruct");
+
+
+var deploymentName = "gpt-4o-mini"; // e.g. "gpt-4o-mini"
+var endpoint = new Uri("https://ai-mikaelglanz6441ai242952280448.services.ai.azure.com/"); // e.g. "https://< your hub name >.openai.azure.com/"
+var apiKey = new ApiKeyCredential(Environment.GetEnvironmentVariable("AZURE_AI_SECRET"));
+
+IChatClient client = new AzureOpenAIClient(
+    endpoint,
+    apiKey)
+.AsChatClient(deploymentName);
 
 var response = await client.GetResponseAsync("What is a Banana? Write only a single sentence.");
 
